7f5603d339e74d01ca24ac3caf4fc8c3bc0a531a
richtrunzo <richtrunzo@gmail.com>
2021-03-15 19:29:48 +0000

    Finalized date function, updated styling, updated search functionality. #3.

--- client/lib/date-check.js
+++ client/lib/date-check.js
@@ -1,27 +1,11 @@
 export default function getDate(date) {
-  const compareObj = {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
-    4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December'
-  };
 
-  const month = new Date(parseInt(date));
-  return month;
-  // const month = new Date(parseInt(date)).getMonth();
-  // const day = new Date(parseInt(date)).getDate();
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
 
-  // for (const prop in compareObj) {
-  //   if (prop === month) {
-  //     return compareObj[prop] + ', ' + day.toString();
-  //   }
-  // }
+  const month = new Date(parseInt(date)).getMonth();
+  const day = new Date(parseInt(date)).getDate();
+
+  return monthNames[month] + ', ' + day.toString();
 }
--- client/pages/coin.jsx
+++ client/pages/coin.jsx
@@ -82,7 +82,6 @@ class Coin extends React.Component {
       .then(res => res.json())
       .then(data => {
         this.setState({ currentCoin: data });
-        console.log(this.state.currentCoin);
       })
       .then(fetch(`https://api.coingecko.com/api/v3/coins/${event.target.id}/market_chart?vs_currency=usd&days=30&interval=daily`, { method: 'GET' })
         .then(res => res.json())
@@ -122,8 +121,23 @@ class Coin extends React.Component {
       .then(res => res.json())
       .then(data => {
         this.setState({ currentCoin: data });
-        console.log(data);
       })
+      .then(fetch(`https://api.coingecko.com/api/v3/coins/${this.state.coinId}/market_chart?vs_currency=usd&days=30&interval=daily`, { method: 'GET' })
+        .then(res => res.json())
+        .then(data => {
+          lineData.labels = [];
+          lineData.datasets[0].data = [];
+          for (let i = 0; i < data.prices.length; i++) {
+            for (let j = 0; j < data.prices[i].length; j++) {
+              if (j === 0) {
+                lineData.labels.push(getDate(data.prices[i][j]));
+              } else if (j === 1) {
+                lineData.datasets[0].data.push(data.prices[i][j]);
+              }
+            }
+          }
+        })
+      )
       .then(() => {
         this.setState({
           coinPage: true
@@ -140,22 +154,24 @@ class Coin extends React.Component {
   renderMarket() {
     return <>
           <div className="d-flex justify-content-center mt-4">
-          <form className="d-flex justify-content-center" onSubmit={this.onFormSubmit}>
-            <input type="text" placeholder="Seach for Coins" onChange={this.onHandleChange}/>
+          <form className="d-flex justify-content-center input-width" onSubmit={this.onFormSubmit}>
+            <input type="text" placeholder="Seach for Coins e.g 'bitcoin'" className="input-width" onChange={this.onHandleChange}/>
             <button>Search</button>
           </form>
+          <div className="input-width d-flex justify-content-center ">
           <label className="ms-3" htmlFor="sort">Sort By:</label>
           <select name="sort" className="ms-3" onChange={this.toggleView}>
             <option>Market Cap</option>
             <option>Volume</option>
           </select>
+          </div>
         </div>
         <div className="d-flex flex-wrap justify-content-center">
           {this.state.coinsM.map((val, index) => {
-            return <div key={index} id={val.id} className="mx-4 mt-3 px-5 border" onClick={this.toggleCoin}>
+            return <div key={index} id={val.id} className="mx-4 mt-3 mb-3 px-5 border cbackground" onClick={this.toggleCoin}>
               <p id={val.id} className="text-center">{val.symbol.toUpperCase()}</p>
-              <img id={val.id} className="mx-auto" src={val.image} width="100"/>
-              <p className="text-center">${val.current_price}</p>
+              <img id={val.id} className="mx-auto" src={val.image} width="100" height="100" />
+              <p className="text-center mt-1">{(val.current_price).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
             </div>;
           }) }
         </div>
@@ -165,22 +181,24 @@ class Coin extends React.Component {
   renderVolume() {
     return <>
           <div className="d-flex justify-content-center mt-4">
-          <form className="d-flex justify-content-center">
-            <input type="text" placeholder="Seach for Coins" />
+          <form className="d-flex justify-content-center input-width" onSubmit={this.onFormSubmit}>
+            <input type="text" placeholder="Seach for Coins e.g 'bitcoin'" className="input-width" onChange={this.onHandleChange}/>
             <button>Search</button>
           </form>
+          <div className="input-width d-flex justify-content-center ">
           <label className="ms-3" htmlFor="sort">Sort By:</label>
           <select name="sort" className="ms-3" onChange={this.toggleView}>
             <option>Market Cap</option>
             <option>Volume</option>
           </select>
+          </div>
         </div>
         <div className="d-flex flex-wrap justify-content-center">
           {this.state.coinsV.map((val, index) => {
-            return <div key={index} id={val.id} className="mx-4 mt-3 px-5 border" onClick={this.toggleCoin}>
+            return <div key={index} id={val.id} className="mx-4 mt-3 mb-3 px-5 border cbackground" onClick={this.toggleCoin}>
               <p className="text-center" id={val.id} onClick={this.toggleCoin}>{val.symbol.toUpperCase()}</p>
               <img className="mx-auto" src={val.image} width="100"/>
-              <p className="text-center">${val.current_price}</p>
+              <p className="text-center">{(val.current_price).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
             </div>;
           }) }
         </div>;
@@ -188,7 +206,6 @@ class Coin extends React.Component {
   }
 
   renderCoin() {
-    console.log(lineData);
     return <div>
             <div className="d-flex" onClick={this.back}>
               <i className="far fa-caret-square-left" onClick={this.back}></i>
@@ -199,10 +216,11 @@ class Coin extends React.Component {
                 <img src={this.state.currentCoin.image.large}/>
               </div>
             <div>
-              <h2>{this.state.currentCoin.name}<span>{`(${this.state.currentCoin.symbol.toUpperCase()})`}</span></h2>
-              <p>Current Price: {this.state.currentCoin.market_data.current_price.usd}<span> 24 Hour Change: </span><span>{this.state.currentCoin.market_data.price_change_24h_in_currency.usd}</span></p>
-              <p>ATH: {this.state.currentCoin.market_data.ath.usd}</p>
-              <p>Market Cap: {this.state.currentCoin.market_data.market_cap.usd}</p>
+              <h2>{this.state.currentCoin.name} <span>{ `(${this.state.currentCoin.symbol.toUpperCase()})`}</span></h2>
+              <p>Current Price: {(this.state.currentCoin.market_data.current_price.usd).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+              <p>24 Hour Change: {(this.state.currentCoin.market_data.price_change_24h_in_currency.usd).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+              <p>ATH: {(this.state.currentCoin.market_data.ath.usd).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+              <p>Market Cap: {(this.state.currentCoin.market_data.market_cap.usd).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
             </div>
           </div>
           <div>
