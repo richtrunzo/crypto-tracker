e99931bd27d25f68c87e9cdabae9a95270144a84
richtrunzo <richtrunzo@gmail.com>
2021-03-15 02:49:42 +0000

    Added functionality to pull data of a single coin when it's clicked. #1.

--- client/pages/coin.jsx
+++ client/pages/coin.jsx
@@ -6,11 +6,14 @@ class Coin extends React.Component {
     this.state = {
       search: false,
       coinId: null,
+      currentCoin: null,
+      coinPage: false,
       coinsM: [],
       coinsV: [],
       renderType: 'm'
     };
     this.toggleView = this.toggleView.bind(this);
+    this.toggleCoin = this.toggleCoin.bind(this);
   }
 
   componentDidMount() {
@@ -40,6 +43,22 @@ class Coin extends React.Component {
     }
   }
 
+  toggleCoin() {
+    this.setState({ coinId: event.target.id });
+    fetch(`https://api.coingecko.com/api/v3/coins/${event.target.id}?tickers=true&market_data=true&community_data=true`, { method: 'GET' })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ currentCoin: data });
+        console.log(this.state.currentCoin);
+      })
+      .then(() => {
+        this.setState({
+          coinPage: true
+        });
+      });
+
+  }
+
   renderMarket() {
     return <>
           <div className="d-flex justify-content-center mt-4">
@@ -55,13 +74,13 @@ class Coin extends React.Component {
         </div>
         <div className="d-flex flex-wrap justify-content-center">
           {this.state.coinsM.map((val, index) => {
-            return <div key={index} className="mx-4 mt-3 px-5 border">
-              <p className="text-center">{val.symbol.toUpperCase()}</p>
-              <img className="mx-auto" src={val.image} width="100"/>
+            return <div key={index} id={val.id} className="mx-4 mt-3 px-5 border" onClick={this.toggleCoin}>
+              <p id={val.id} className="text-center">{val.symbol.toUpperCase()}</p>
+              <img id={val.id} className="mx-auto" src={val.image} width="100"/>
               <p className="text-center">{val.current_price}</p>
             </div>;
           }) }
-        </div>;
+        </div>
         </>;
   }
 
@@ -90,11 +109,36 @@ class Coin extends React.Component {
         </>;
   }
 
+  renderCoin() {
+    return <div>
+            <div className="d-flex">
+              <i className="far fa-caret-square-left"></i>
+              <p>Go Back</p>
+            </div>
+            <div className="d-flex">
+              <div>
+                <img src={this.state.currentCoin.image.large}/>
+              </div>
+            <div>
+              <h2>{this.state.currentCoin.name}<span>{`(${this.state.currentCoin.symbol.toUpperCase()})`}</span></h2>
+              <p>Current Price: {this.state.currentCoin.market_data.current_price.usd}<span> 24 Hour Change: </span><span>{this.state.currentCoin.market_data.price_change_24h_in_currency.usd}</span></p>
+              <p>ATH: {this.state.currentCoin.market_data.ath.usd}</p>
+              <p>Market Cap: {this.state.currentCoin.market_data.market_cap.usd}</p>
+            </div>
+          </div>
+          <div>
+            <p dangerouslySetInnerHTML={{ __html: this.state.currentCoin.description.en }}></p>
+          </div>
+        </div>;
+  }
+
   render() {
-    if (this.state.renderType === 'm') {
+    if (this.state.renderType === 'm' && this.state.coinPage === false) {
       return this.renderMarket();
-    } else {
+    } else if (this.state.renderType === 'v' && this.state.coinPage === false) {
       return this.renderVolume();
+    } else if (this.state.coinPage === true) {
+      return this.renderCoin();
     }
   }
 
