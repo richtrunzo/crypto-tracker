92e9721d1ca6c94800bd6eaebbd628ed5bb3a3ec
richtrunzo <richtrunzo@gmail.com>
2021-03-25 03:50:43 +0000

    Added a new test route in node.js

--- server/index.js
+++ server/index.js
@@ -67,6 +67,15 @@ app.get('/api/date/:coin/:date', (req, res) => {
     });
 });
 
+app.get('/api/date/:coin', (req, res) => {
+  const { coin } = req.params;
+  fetch(`https://api.coingecko.com/api/v3/coins/${coin}`, { method: 'GET' })
+    .then(res => res.json())
+    .then(data => {
+      res.json(data.market_data.current_price.usd);
+    });
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
