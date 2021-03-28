0fece5550c732c762f6f1978bf259ec318d7252b
richtrunzo <richtrunzo@gmail.com>
2021-03-28 00:30:17 +0000

    Updated nav bar component.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Tick from './ticker';
 
 function Navbar(props) {
+
+  const [page, setPage] = useState(false);
+
+  const onClick = () => {
+    if (page === false) {
+      setPage(true);
+    } else {
+      setPage(false);
+    }
+  };
+
+  const Burger = () => {
+    if (page === false) {
+      return <i onClick={onClick} className="fas fa-bars"></i>;
+    } else {
+      return <>
+        <div>
+          <i onClick={onClick} className="back-text fas fa-bars"></i>
+          <a href="#"><p className="text mb-5">Coins</p></a>
+          <a href="#news"><p className="text mb-5">News</p></a>
+          <a href="#fomo"><p className="text mb-5">FOMO</p></a>
+        </div>
+        </>;
+    }
+  };
+
   return <div>
     <div className="d-flex nwidth black">
       <div className="navwidth pt-5 text-center">
@@ -10,16 +37,11 @@ function Navbar(props) {
           <p className="text ms-2">Crypto Tracker</p>
         </div>
       </div>
-      <div>
-      </div>
-      <div className="navwidth text-center pt-5 d-flex justify-content-around">
-        <a href="#"><p className="text mb-5">Coins</p></a>
-        <a href="#news"><p className="text mb-5">News</p></a>
-        <a href="#fomo"><p className="text mb-5">FOMO</p></a>
-      </div>
+      <Burger />
     </div>
     <Tick />
-  </div>;
+    </div>;
+
 }
 
 export default Navbar;
