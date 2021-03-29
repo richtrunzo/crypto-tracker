915e4e8a3717f8bd9ff91bee1764575448b881e8
richtrunzo <richtrunzo@gmail.com>
2021-03-29 21:54:30 +0000

    Updated news page font sizes.

--- client/pages/news.jsx
+++ client/pages/news.jsx
@@ -76,9 +76,9 @@ export default class News extends React.Component {
 
   renderAllNews() {
     return <>
-          <form className="d-flex justify-content-center mt-4 input-width" onSubmit={this.onFormSubmit}>
-            <label className="ms-3" htmlFor="sort">View News By Coin:</label>
-            <select defaultValue="All News" name="sort" className="ms-3" onChange={this.onHandleChange}>
+          <form className="font d-flex justify-content-center mt-4 input-width" onSubmit={this.onFormSubmit}>
+            <label className="font ms-3" htmlFor="sort">View News By Coin:</label>
+            <select defaultValue="All News" name="sort" className="font ms-3" onChange={this.onHandleChange}>
                <option>All News</option>
                 {this.state.coins.map((val, index) => {
                   return <option key={index}>{val.name}</option>;
@@ -89,8 +89,8 @@ export default class News extends React.Component {
           <div className="d-flex flex-column align-items-center mx-5 mb-4">
             {this.state.allNews.articles.map((val, index) => {
               return <div key={index} className="mt-4 border mx-2 cbackground news-width" >
-                            <p className="text-center white-text pt-2">{this.state.allNews.articles[index].source.name}</p>
-                            <a href={this.state.allNews.articles[index].url}><p className="text-center white-text pt-3">{this.state.allNews.articles[index].title}</p></a>
+                            <p className="fw-bold fs-1 font text-center white-text pt-2">{this.state.allNews.articles[index].source.name}</p>
+                            <a href={this.state.allNews.articles[index].url}><p className="fst-italic font text-center white-text pt-3">Link: <span>{this.state.allNews.articles[index].title}</span></p></a>
                           <div className="d-flex justify-content-center flex-wrap mb-4">
                             <EmailShareButton className="mx-2" url={this.state.allNews.articles[index].url}>
                               <EmailIcon size={35} round/>
@@ -116,44 +116,37 @@ export default class News extends React.Component {
 
   renderSearchNews() {
     return <>
-          <form className="d-flex justify-content-center input-width" onSubmit={this.onFormSubmit}>
-            <label className="ms-3" htmlFor="sort">View News By Coin:</label>
-            <select defaultValue="All News" name="sort" className="ms-3" onChange={this.onHandleChange}>
-                <option>All News</option>
+          <form className="d-flex font justify-content-center mt-4 input-width" onSubmit={this.onFormSubmit}>
+            <label className="font ms-3" htmlFor="sort">View News By Coin:</label>
+            <select defaultValue="All News" name="sort" className="font ms-3" onChange={this.onHandleChange}>
+               <option>All News</option>
                 {this.state.coins.map((val, index) => {
-                  if (index < 51) {
-                    return <option key={index}>{val.name}</option>;
-                  }
+                  return <option key={index}>{val.name}</option>;
                 })}
             </select>
             <button>Submit</button>
           </form>
-          <div className="d-flex flex-column justify-content-center mx-5 mb-4">
-            {this.state.searchNews.articles.map((val, index) => {
-              return <div key={index} className="col d-flex mt-4 border mx-2 cbackground" >
-                        <div>
-                          <img className="ms-4 mt-4 mb-4 border" src={this.state.searchNews.articles[index].urlToImage} width="300" height="150"/>
-                        </div>
-                        <div className="ms-5 col-6">
-                          <p className="text-center white-text pt-2">{this.state.searchNews.articles[index].source.name}</p>
-                          <a href={this.state.searchNews.articles[index].url}><p className="text-center white-text pt-3">{this.state.searchNews.articles[index].title}</p></a>
-                          <div className="d-flex justify-content-center">
-                            <EmailShareButton className="mx-2" url={this.state.allNews.articles[index].url}>
-                              <EmailIcon size={32} round/>
+          <div className="d-flex flex-column align-items-center mx-5 mb-4">
+            {this.state.allNews.articles.map((val, index) => {
+              return <div key={index} className="mt-4 border mx-2 cbackground news-width" >
+                            <p className="fw-bold fs-1 font text-center white-text pt-2">{this.state.searchNews.articles[index].source.name}</p>
+                            <a href={this.state.searchNews.articles[index].url}><p className="fst-italic font text-center white-text pt-3">{this.state.searchNews.articles[index].title}</p></a>
+                          <div className="d-flex justify-content-center flex-wrap mb-4">
+                            <EmailShareButton className="mx-2" url={this.state.searchNews.articles[index].url}>
+                              <EmailIcon size={35} round/>
                             </EmailShareButton>
-                            <FacebookShareButton className="mx-2" url={this.state.allNews.articles[index].url}>
-                              <FacebookIcon size={32} round/>
+                            <FacebookShareButton className="mx-2" url={this.state.searchNews.articles[index].url}>
+                              <FacebookIcon size={35} round/>
                             </FacebookShareButton>
-                            <LinkedinShareButton className="mx-2" url={this.state.allNews.articles[index].url}>
-                              <LinkedinIcon size={32} round/>
+                            <LinkedinShareButton className="mx-2" url={this.state.searchNews.articles[index].url}>
+                              <LinkedinIcon size={35} round/>
                             </LinkedinShareButton>
-                            <RedditShareButton className="mx-2" url={this.state.allNews.articles[index].url}>
-                              <RedditIcon size={32} round/>
+                            <RedditShareButton className="mx-2" url={this.state.searchNews.articles[index].url}>
+                              <RedditIcon size={35} round/>
                             </RedditShareButton>
-                            <TwitterShareButton className="mx-2" url={this.state.allNews.articles[index].url}>
-                              <TwitterIcon size={32} round/>
+                            <TwitterShareButton className="mx-2" url={this.state.searchNews.articles[index].url}>
+                              <TwitterIcon size={35} round/>
                             </TwitterShareButton>
-                          </div>
                         </div>
                     </div>;
             })}
