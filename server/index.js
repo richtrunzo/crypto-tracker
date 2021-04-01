773599630675bfe6a1d91722d13fdb205dd267e6
richtrunzo <richtrunzo@gmail.com>
2021-04-01 18:12:42 +0000

    updated server requirements.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv/config');
+// require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const fetch = require('node-fetch');
