4eca2ae645da9054903039ab16e04f8e555bc40b
richtrunzo <richtrunzo@gmail.com>
2021-04-02 16:47:58 +0000

    Added conditional rendering to the menu so the user knows which page they are currently on, also added a media query to shorten the footer when it's clicked.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,7 +6,26 @@ function Navbar(props) {
 
   const [page, setPage] = useState(false);
 
+  let coin = 'white-text underline';
+  let news = 'white-text underline';
+  let fomo = 'white-text underline';
+
+  if (location.hash === '') {
+    coin = 'red-text';
+    news = 'white-text underline';
+    fomo = 'white-text underline';
+  } else if (location.hash === '#news') {
+    coin = 'white-text underline';
+    news = 'red-text';
+    fomo = 'white-text underline';
+  } else if (location.hash === '#fomo') {
+    coin = 'white-text underline';
+    news = 'white-text underline';
+    fomo = 'red-text';
+  }
+
   const onClick = () => {
+    console.log(location.hash);
     if (page === false) {
       setPage(true);
     } else {
@@ -21,10 +40,10 @@ function Navbar(props) {
       return <>
       <div className="col-4 mt-5 text-center">
         <div className="forward-menu position-fixed top-50 start-50 translate-middle mx-auto">
-            <p className="menu-font mb-2 font mt-1 px-1 red-text bold-font">Menu</p>
-            <a href="#" onClick={onClick}><p className="menu-font mb-2 font mt-5 px-1 white-text">Coins</p></a>
-            <a href="#news"onClick={onClick}><p className="menu-font font mb-2 mt-5 px-1 white-text">News</p></a>
-            <a href="#fomo"onClick={onClick}><p className="menu-font font mb-2 mt-5 px-1 white-text">FOMO</p></a>
+            <p className="menu-font mb-2 font mt-1 px-1 white-text bold-font">Menu</p>
+            <a href="#" onClick={onClick}><p className={`menu-font mb-2 font mt-5 px-1 ${coin}`}>Coins</p></a>
+            <a href="#news"onClick={onClick}><p className={`menu-font mb-2 font mt-5 px-1 ${news}`}>News</p></a>
+            <a href="#fomo"onClick={onClick}><p className={`menu-font mb-2 font mt-5 px-1 ${fomo}`}>FOMO</p></a>
             <i onClick={onClick} className="pointer menu-font mt-5 white-text fas fa-times"></i>
         </div>
         </div>
